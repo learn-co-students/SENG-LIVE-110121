8a230beaa32b3b4c06290f25d23700aea371a425
ixnp <enprocopios@gmail.com>
2021-12-16 09:58:54 -0800

    04-SQL-and-ORM/

---
+++ phase-3/04-SQL-and-ORM/app/models/orm.rb
@@ -0,0 +1,57 @@
+class PatientORM
+    attr_accessor :name, :age, :owner, :number
+    attr_reader :species, :id
+
+    def initialize(attributes) 
+        attributes.each do |key, value| 
+            if self.respond_to?("#{key.to_s}=") 
+                self.send("#{key.to_s}=", value) 
+            end 
+        end
+
+    end
+
+    def save
+        sql = <<-SQL
+        INSERT INTO patient (species, name, age, owner, number) VALUES (?,?,?,?,?);        
+        SQL
+        DB.execute(sql, self.species, self.name, self.age, self.owner, self.number)
+        @id = DB.last_insert_row_id
+        self 
+    end
+
+    def self.create(args)
+        patient = PatientORM.new(args)
+        patient.save
+        patient
+      end
+
+
+    def self.all 
+        resources = DB.execute("SELECT * FROM patient")
+         m = resources.map do |p|
+           puts p
+          self.new(p)
+        end
+        puts m
+        m
+    end
+
+    def self.create_table 
+        sql = <<-SQL
+        CREATE TABLE IF NOT EXISTS patient (
+            id INTEGER,
+            clinic_id INTEGER,
+            species VARCHAR(30),
+            name VARCHAR(50),
+            age INTEGER,
+            owner VARCHAR(50),
+            number VARCHAR(15),
+            PRIMARY KEY(id)
+        );
+        SQL
+        DB.execute(sql)
+    end 
+
+
+end 
