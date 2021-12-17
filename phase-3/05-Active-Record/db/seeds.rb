d86c8fb629341db838c66437c5ff11c8101081aa
ixnp <enprocopios@gmail.com>
2021-12-17 09:48:32 -0800

    starter

---
+++ phase-3/05-Active-Record/db/seeds.rb
@@ -0,0 +1,11 @@
+
+
+c1 = Clinic.create({clinic_name:'puppersRus', address:'5555 6th Seattle Wa 00000', phone:'(999)999-9999'})
+c2 = Clinic.create({clinic_name:'Seattle pets clinic', address:'5555 6th Seattle Wa 00000', phone:'(999)999-9999'})
+
+Patient.create({species:'cat', name:'rose', age:10, owner:'ix', phone:'999999999', active:true, clinic_id:c1.id})
+Patient.create({species:'cat', name:'bob', age:9, owner:'tom', phone:'999999999', active:true, clinic_id:c1.id})
+Patient.create({species:'dog', name:'dan', age:2, owner:'max', phone:'999999999', active:true, clinic_id:c1.id})
+Patient.create({species:'bird', name:'picard', age:2, owner:'oz', phone:'999999999', active:true, clinic_id:c2.id})
+Patient.create({species:'rabit', name:'ted', age:1, owner:'zac', phone:'999999999', active:true, clinic_id:c2.id})
+puts 'done seeding'
