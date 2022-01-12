893667172ca696035ff314317ba4780a6ed6480a
Aysan Isayo <aisayo@icloud.com>
2022-01-12 08:47:45 -0800

    fixed spelling mistake on validation

--- phase-4/03-client-server-comm-2/marketplace/marketplace-api/app/models/category.rb
+++ phase-4/03-client-server-comm-2/marketplace/marketplace-api/app/models/category.rb
@@ -2,5 +2,5 @@ class Category < ApplicationRecord
     has_many :category_items
     has_many :items, through: :category_items
 
-    validates :name, presence: true, uniquness: true
+    validates :name, presence: true, uniqueness: true
 end
