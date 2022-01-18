e1ef61f47b95c07862bed45ec08536e3dc030dcb
Aysan Isayo <aisayo@icloud.com>
2022-01-18 07:04:17 -0800

    lecture starter code

---
+++ phase-4/05-authentication/marketplace/marketplace-api/app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+
+    def create 
+        user = User.create!(user_params)
+        render json: user, status: :created 
+    end
+
+    def destroy 
+        user = User.find(params[:id])
+        user.destroy 
+        head :no_content 
+    rescue ActiveRecord::RecordNotFound => error 
+       render json: {error: error.message}, status: :not_found
+    end
+
+    private 
+
+    def user_params
+        params.permit(:username, :email)
+    end
+end
